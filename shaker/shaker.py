d2af67ef6d5993c55f284a81afa6d73cf700c012
Joseph Orgill (staff) <Joseph.Orgill@nottingham.ac.uk>
2025-12-16 16:45:59 +0000

    updated sheker with folder

--- shaker/shaker.py
+++ shaker/shaker.py
@@ -1,19 +1,19 @@
-
 from .settings import SHAKER_ARDUINO
 from labequipment.arduino import Arduino
 import numpy as np
 import time
+from datetime import datetime
 import sys
-import glob
 import os
+import shutil
+from shaker.settings import SETTINGS_PATH, TRACK_LEVEL, ACCELEROMETER_FILE, ACCELEROMETER_SHAKER
+from labequipment.accelerometer import pk_acceleration
 sys.path.insert(0, '..')
 
 
 class Shaker:
     """Shaker class handles communication between pc and Red Shaker. It can be used
     to perform simple operations:
-    
-    change
 
     1) Open communication
     2) Set a particular drive amplitude
@@ -29,22 +29,66 @@ class Shaker:
 
     """
 
-    def __init__(self, duty_filename = None):
+    def __init__(self, folder = None, acc_arduino = False):
         print("shaker init")
         self.power = Arduino(SHAKER_ARDUINO)
         time.sleep(1)
-        #self.power.read_all()
-        self.power.send_serial_line('h')
+        self.power.read_all()
         self.switch_serial_mode()
-        self.recording = False
-        self.filecount = 0
+        # define accelerometer if acc_arduino is True, allowing acceleration to be measured
+        if acc_arduino:
+            self.acc = Arduino(ACCELEROMETER_SHAKER)
+        else:
+            self.acc = None
         
-        self.duty_filename = duty_filename
-        if duty_filename is not None:
-            #raises error if file already exists
-            self.check_dutyfilename_doesexist()
-            self.filename_update()
-       
+        self.folder = folder
+        self.creation_time = time.time() 
+        if folder is not None:
+            self._make_dir()
+        else:
+            print('no filename provided, metadata will not be saved')
+        self.counter = 0
+
+    def _make_dir(self):
+        """
+        generates experiment folder if it doesnt already exist          
+        """
+        if os.path.isdir(self.folder):
+            raise Exception(f"No folder created. '{self.folder}' already exists.")
+        else:
+            os.makedirs(self.folder)
+            print(f"Successfully created '{self.folder}'.")
+            self._make_meta()
+            self._save_calibrations()
+
+    def _save_calibrations(self):
+        """
+        Save accelerometer callibration to experiment folder
+        """
+        file_name = 'calibrations.txt'
+        full_path = os.path.join(self.folder, file_name)
+        shutil.copy(SETTINGS_PATH + ACCELEROMETER_FILE , full_path)
+    
+    def _make_meta(self):
+        """
+        Creates metadata txt file, adds time of creation and column heading
+        """
+        file_name = 'MetaData.txt'
+        full_path = os.path.join(self.folder, file_name)
+        now = datetime.now()
+        formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        file_content = (f"Shaker instance created at: {formatted_time}")
+        file_header = "\ntime_since_creation  counter  duty  recording   Acceleration"
+
+        try:
+            with open(full_path, 'w') as file:
+                file.write(file_content)
+                file.write(file_header)
+                print(f"Successfully written to: {full_path}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        self.full_path = full_path
+    
 
     def switch_serial_mode(self):
         """Put shaker in serial mode"""
@@ -62,50 +106,59 @@ class Shaker:
 
         val is a 3 digit number indicating new duty cycle
         """
-        self.save_duty(val)
         string = 'd{:03}'.format(val)
         self.power.send_serial_line(string)
 
+        
+
         self._clear_buffer()
+        
+    def _record_to_meta(self, file, val, description):
+        """
+        add data to the meta data file
+        doesnt record acc for a ramp (sequence) because getting acc from accelerometer is too slow
+        
+        :param file: file name passed from set_duty_and_record or sequence, where the file is also opened
+        :param val: Duty cycle
+        :param description: camera status. start and stop for set_duty_and_record, sequence during a ramp
+        """
+        t = time.time() - self.creation_time
+        
+        if self.acc is not None and description != 'sequence':
+            peak = pk_acceleration(self.acc)
+            info = f"{t:.4f} {self.counter} {val} {description} {peak}"
+        else:
+            info = f"{t:.4f} {self.counter} {val} {description}"
+        
+        try:
+            file.write('\n')
+            file.write(info)
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def set_duty_and_record(self, val: int):
         """Sets new duty cycle but also sends a TTL signal to camera output 
             to trigger camera. This starts or stops the camera recording as appropriate.
 
             Works with Panasonic HC-X1000 and probably others
+
+            If folder is provided _record_to_meta adds to meta data file
         """
-        self.save_duty(val)
-        self.recording = not self.recording
         string = 'i{:03}'.format(val)
         self.power.send_serial_line(string)
 
-        self._clear_buffer()
-        if not self.recording and self.duty_filename is not None:
-            self.filecount += 1
-            self.filename_update()
-    
-    def save_duty(self,val):
-        if self.duty_filename is not None:
-            with open(self.duty_filename, 'a') as f:
-                f.write(f"{time.time()} {val}\n")
-
-    def filename_update(self):
-        counter = str(self.filecount)
-        number = (4 - len(counter)) * '0' + counter
-        if '_' in self.duty_filename:
-            base = self.duty_filename.split('_')[0]
-        else:
-            base=self.duty_filename.split('.')[0]
-        self.duty_filename = base + '_' + number + '.' + self.duty_filename.split('.')[1]
-
-    def check_dutyfilename_doesexist(self):
-        files = glob.glob(self.duty_filename.split('.')[0]+'*')
-        if files:
-            raise FileExistsError(
-                f"Duty filename {self.duty_filename} already exists. Please change the filename to avoid overwriting existing data.")
-
+        if self.folder is not None:
                 
+            if self.counter % 2 == 0:
+                recording = 'start'
+            else:
+                recording = 'stop'        
 
+            with open(self.full_path, 'a') as file:
+                self._record_to_meta(file, val, recording)              
+
+        self._clear_buffer()
+        self.counter += 1
 
     def ramp(self,
              start: int,
@@ -138,6 +191,8 @@ class Shaker:
                  stop_at_end: bool = False):
         """Apply duty_cycle values sequentially from list of values
 
+        if folder is provided, data is writen to meta at every step
+
         Args:
             values (list[int]): sequential list of duty_cycle values to be applied. Must hav
             rate (float): number of values per second
@@ -147,17 +202,32 @@ class Shaker:
         self.set_duty_and_record(
             values[0]) if record else self.set_duty(values[0])
         delay = 1/rate
-        time.sleep(delay)
 
-        if len(values) > 1:
-            for duty_cycle in values[1:]:
-                t = time.time()
-                self.set_duty(duty_cycle)
-                interval = delay - time.time() + t
-                if interval > 0:
-                    time.sleep(interval)
-                else:
-                    print('Rate too high, timing will not be accurate')
+        
+
+        try:
+            if self.folder is not None:
+                
+                file=open(self.full_path, 'a')
+
+            time.sleep(delay)
+
+            if len(values) > 1:
+                for duty_cycle in values[1:]:
+                    t = time.time()
+                    self.set_duty(duty_cycle)
+                    if self.folder is not None:
+                        self._record_to_meta(file, duty_cycle, "sequence")   
+                    interval = delay - time.time() + t
+                    if interval > 0:
+                        time.sleep(interval)
+                    else:
+                        print('Rate too high, timing will not be accurate')
+        except:
+            pass
+        finally:
+            if self.folder is not None:
+                file.close()
 
         if stop_at_end:
             self.set_duty_and_record(0) if record else self.set_duty(0)
